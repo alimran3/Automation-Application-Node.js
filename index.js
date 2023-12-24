26c160291cab3bf5017a2e969ceb77d88d8230d6
AL Imran <alispower77@gmail.com>
2023-12-24 16:42:55 +0600

    js file name change

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http=require('http');
 const server = http.createServer((req,res)=>{
-    if(req.url=='/')
+    if(req.url==='/')
     {
-        res.write('You have opended a page');
+        res.write('You have opened a page');
     }
-    else if(req.url=='/about us')
+    else if(req.url==='/about us')
          res.write('This is about us page');
     else
          res.write('This page is not available');
@@ -12,5 +12,5 @@ const server = http.createServer((req,res)=>{
          res.end();
 })
 
-server.listen(3000);
-console.log("listening on port 3000");
+server.listen(3099);
+console.log("listening on port 3099");
